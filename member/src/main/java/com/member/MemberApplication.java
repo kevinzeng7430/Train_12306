efee4d1f8a0cf92924f8956c6f94b49c69022688
kevinzeng7430 <2472464767@qq.com>
2025-06-05 16:21:40 +0800

    修改启动日志，增加/member，修改logback

--- member/src/main/java/com/member/MemberApplication.java
+++ member/src/main/java/com/member/MemberApplication.java
@@ -1,11 +1,22 @@
 package com.member;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+
+
 
 @SpringBootApplication
+@Slf4j
 public class MemberApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功!");
+        log.info("地址:\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
