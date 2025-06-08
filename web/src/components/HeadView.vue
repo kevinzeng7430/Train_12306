133235cdad68712bac9e47b76e5d01b6f10b53bf
kevinzeng7430 <2472464767@qq.com>
2025-06-08 22:54:21 +0800

    增加HeadView和SideView两个组件

---
+++ web/src/components/HeadView.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-layout-header class="header" id="components-layout-demo-top-side-2">
+    <div class="logo"/>
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 111</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+
+</template>
+
+<script>
+import {defineComponent, ref,} from 'vue';
+
+
+export default defineComponent({
+  name: "head-view",
+  setup() {
+
+
+    return {
+      selectedKeys1: ref(['2']),
+    };
+  },
+});
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
