b04a5a07a7acf8bc7ee713ad6c7aec25bb3e4efe
kevinzeng7430 <2472464767@qq.com>
2025-06-08 15:16:08 +0800

    增加login路由以及页面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,18 +2,31 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
+    /*
+    两种路由方式：
+    1. 静态路由：打包的时候全部打包，所以打包的项目会比较大
+    2. 动态路由：懒加载，需要用的时候再加载，打包的项目会比较小
+     */
   {
+    // 方法1：
     path: '/',
     name: 'home',
     component: HomeView
+
   },
   {
+    // 方法2：懒加载
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
   }
 ]
 
