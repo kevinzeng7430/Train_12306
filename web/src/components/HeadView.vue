7dbed50edf304a896569b51afaecd8a94b4978dd
kevinzeng7430 <2472464767@qq.com>
2025-06-12 22:13:06 +0800

    利用vue(store)缓存信息

--- web/src/components/HeadView.vue
+++ web/src/components/HeadView.vue
@@ -1,6 +1,12 @@
 <template>
   <a-layout-header class="header" id="components-layout-demo-top-side-2">
     <div class="logo"/>
+    <div style="float: right; color: white">
+      你好：{{ member.mobile}} &nbsp;&nbsp;
+      <router-link to="/login">
+        <a-button type="primary" size="small" style="color: white">退出登录</a-button>
+      </router-link>
+    </div>
     <a-menu
         v-model:selectedKeys="selectedKeys1"
         theme="dark"
@@ -17,15 +23,17 @@
 
 <script>
 import {defineComponent, ref,} from 'vue';
+import store from "@/store";
 
 
 export default defineComponent({
   name: "head-view",
   setup() {
-
+    let member = store.state.member
 
     return {
       selectedKeys1: ref(['2']),
+      member
     };
   },
 });
