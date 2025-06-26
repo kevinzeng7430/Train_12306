6be8b06783ab27467a871367802187c4e11f6074
kevinzeng7430 <2472464767@qq.com>
2025-06-26 21:50:56 +0800

    增加batch定时任务处理模块

---
+++ batch/src/main/java/com/kz/business/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.kz.business.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
+public class TestController {
+    @GetMapping("/hello")
+    public String test() {
+        return "Hello BatchApplication";
+    }
+}
